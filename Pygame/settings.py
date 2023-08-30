7923850c7069b7d8343c2e457400f6e20be0546f
Dipu Singha <hunabopa@gmail.com>
2023-08-30 12:55:59 +0600

    tesing pygame as opposed to arcade and raylib

---
+++ Pygame/settings.py
@@ -0,0 +1,10 @@
+import pathlib
+from random import randrange, uniform
+
+WIN_SIZE = WIN_W, WIN_H = 1000, 600
+SPRITE_DIR_PATH = 'assets/sprites'
+
+FONT_SIZE = 20
+SPEED = 600
+NUM_SPRITES_PER_CLICK = 10
+NUM_ANGLES = 180
