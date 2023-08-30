4a4b5a6a46513788ef022cb13bb68ec69581eae4
Dipu Singha <hunabopa@gmail.com>
2023-08-30 16:01:38 +0600

    pyray library

--- pygame_test.py
+++ pygame_test.py
@@ -1,14 +1,4 @@
-import pathlib
-from random import randrange, uniform
-
-WIN_SIZE = WIN_W, WIN_H = 1000, 600
-SPRITE_DIR_PATH = 'assets/sprites'
-
-FONT_SIZE = 20
-SPEED = 600
-NUM_SPRITES_PER_CLICK = 10
-NUM_ANGLES = 18
-
+from settings import *
 import pygame as pg
 import pygame.freetype as ft
 import sys
